94bcd98cd7685376e920e43ee6d02ee44a632dc0
marianaoliveira1 <marianaoliveirafernandes19@gmail.com>
2024-03-05 14:20:04 -0300

    tela de resultados

--- lib/pages/quiz/widgtes/result_quiz.dart
+++ lib/pages/quiz/widgtes/result_quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:geografia/pages/home/home_page.dart';
 import 'package:geografia/utils/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,39 +15,78 @@ class ResultQuiz extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      backgroundColor: DefaultColors.bege,
-      title: Text(
-        "Resultado",
-        style: GoogleFonts.roboto(
-          color: DefaultColors.title,
-          fontSize: 18.sp,
-          fontWeight: FontWeight.w400,
-        ),
-      ),
-      content: Text(
-        "Você acertou $result de $questionLength questões",
-        style: GoogleFonts.roboto(
-          color: DefaultColors.title,
-          fontSize: 16.sp,
-          fontWeight: FontWeight.w400,
-        ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text(
-            "Ok",
-            style: GoogleFonts.roboto(
-              color: DefaultColors.title,
-              fontSize: 16.sp,
-              fontWeight: FontWeight.w400,
-            ),
+    return Scaffold(
+      backgroundColor: DefaultColors.roxo,
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(20.w),
+          height: 300.h,
+          width: 300.w,
+          decoration: BoxDecoration(
+            color: DefaultColors.branco,
+            borderRadius: BorderRadius.circular(16.h),
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 15.h),
+              Text(
+                'Resultado',
+                style: GoogleFonts.inconsolata(
+                  color: DefaultColors.black,
+                  fontSize: 22.sp,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              SizedBox(height: 30.h),
+              if (result > 5) Image.asset('assets/icons/sad.png', height: 60.h) else Image.asset('assets/icons/sunglass.png', height: 60.h),
+              SizedBox(height: 30.h),
+              Text(
+                "Você acertou $result de $questionLength questões",
+                style: GoogleFonts.roboto(
+                  color: DefaultColors.title,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 40.h),
+              Container(
+                decoration: BoxDecoration(
+                  color: DefaultColors.roxo,
+                  borderRadius: BorderRadius.circular(14),
+                ),
+                width: double.infinity,
+                padding: EdgeInsets.only(
+                  bottom: 10.h,
+                  top: 10.h,
+                ),
+                margin: EdgeInsets.only(
+                  left: 20.h,
+                  right: 20.h,
+                ),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomePage(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "voltar para o inicio",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.roboto(
+                      color: DefaultColors.bege,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
